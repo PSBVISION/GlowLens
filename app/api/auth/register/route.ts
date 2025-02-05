b3a2651d2da220f5450bd63cb694b1c95d8d0b2e
psbvision <punitsinghbisht1529@gmail.com>
2025-02-05 23:17:26 +0530

    user connected to database

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -6,11 +6,30 @@ export async function POST(request: NextRequest) {
   try {
     const {email, password} = await request.json();
     if(!email || !password){
-      return NextResponse.json({
-        
-      })
+      return NextResponse.json(
+        {error: "Email and password is required"},
+        {status: 400}
+      )
     }
+
+    await ConnectToDatabase();
+
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+      return NextResponse.json(
+        {error: "Email is already in use"},
+        {status: 400}
+      )
+    }
+    await User.create({email, password});
+    return NextResponse.json(
+      {message: "User registered successfully"},
+      {status: 201}
+    );
   } catch (error) {
-    
+    return NextResponse.json(
+      {error: "Failed to register user"+ error},
+      {status: 500}
+    );
   }
 }
