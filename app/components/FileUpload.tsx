ec8988b7012b1947158633d1330be34e7742a5a4
psbvision <punitsinghbisht1529@gmail.com>
2025-02-18 22:24:02 +0530

    file uploading approxxly complete

--- app/components/FileUpload.tsx
+++ app/components/FileUpload.tsx
@@ -4,6 +4,7 @@ import { ImageKitProvider, IKUpload } from "imagekitio-next";
 import { Loader2 } from "lucide-react";
 import { IKUploadResponse } from "imagekitio-next/dist/types/components/IKUpload/props";
 
+
 interface FileUploadProps {
   onSuccess: (res: IKUploadResponse)=> void
   onProgress? : (progress: number) => void;
@@ -24,56 +25,70 @@ export default function FileUpload({
 }: FileUploadProps) {
   const [uploading, setUploading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const onError = (err) => {
+  const onError = (err: {message: string}) => {
     console.log("Error", err);
+    setError(err.message);
+    setUploading(false);
   };
   
-  const onSuccess = (res) => {
+  const handleSuccess = (res: IKUploadResponse) => {
     console.log("Success", res);
+    setUploading(false);
+    setError(null);
+    onSuccess(res);
   };
   
-  const onUploadProgress = (progress) => {
-    console.log("Progress", progress);
+  const handleProgress = (evt : ProgressEvent) => {
+    if(evt.lengthComputable && onProgress){
+      const percentComplete = (evt.loaded /evt.total) * 100;
+      onProgress(Math.round(percentComplete));
+    }
   };
   
-  const onUploadStart = (evt) => {
-    console.log("Start", evt);
+  const handleStartUpload = () => {
+    setUploading(true)
+    setError(null);
   };
+
+  const validateFile = (file: File) => {
+    if(fileType === "video"){
+      if(!file.type.startsWith("video/")){
+        setError("Please Upload Video File")
+      }
+      if(file.size > 100 * 1024 * 1024){
+        setError("File size should be less than 100MB")
+        return false
+      }
+    }else{
+      const validTypes = ["image/jpeg", "image/png", "image/webp"];
+      if(!validTypes.includes(file.type)){
+        setError("Please Upload Image with valid format")
+        return false
+      }
+      if(file.size > 5 * 1024 * 1024){
+        setError("File size should be less than 5MB")
+        return false
+      }
+    }
+  }
+  return false
+
+  }
+
   return (
-    <div className="App">
-      <h1>ImageKit Next.js quick start</h1>
+    <div className="space-y-2">
       
-        <p>Upload an image with advanced options</p>
         <IKUpload
-          fileName="test-upload.jpg"
-          tags={["sample-tag1", "sample-tag2"]}
-          customCoordinates={"10,10,10,10"}
-          isPrivateFile={false}
+          fileName={fileType === "video" ? "video" : "image"}
+          
+          
           useUniqueFileName={true}
-          responseFields={["tags"]}
-          validateFile={(file) => file.size < 2000000}
-          folder={"/sample-folder"}
-          {/* extensions={[
-            {
-              name: "remove-bg",
-              options: {
-                add_shadow: true,
-              },
-            },
-          ]} */}
-          webhookUrl="https://www.example.com/imagekit-webhook" // replace with your webhookUrl
-          overwriteFile={true}
-          overwriteAITags={true}
-          overwriteTags={true}
-          overwriteCustomMetadata={true}
-          {/* customMetadata={{
-            "brand": "Nike",
-            "color": "red",
-          }} */}
+          
+          validateFile={validateFile}
           onError={onError}
-          onSuccess={onSuccess}
-          onUploadProgress={onUploadProgress}
-          onUploadStart={onUploadStart}
+          onSuccess={handleSuccess}
+          onUploadProgress={handleProgress}
+          onUploadStart={handleStartUpload}
           transformation={{
             pre: "l-text,i-Imagekit,fs-50,l-end",
             post: [
